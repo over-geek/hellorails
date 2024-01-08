6f0c5f1a2bb4c0137c23ff90158f10369d98b0e3
over-geek <kenagy100@gmail.com>
2024-01-08 17:28:05 +0000

    Update link using link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
-  get 'pages/hello'
+  resources :articles
 end
